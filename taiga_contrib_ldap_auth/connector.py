253d4bcb1f81c5fab55fb7d1be88f8cb4ed981ab
Noel Maersk <veox@users.noreply.github.com>
2016-11-29 19:13:51 +0200

    Rename PROPERTY to ATTRIBUTE, format and update README.
    What's previously been called properties is properly called attributes:
    
    https://www.ldap.com/glossary-of-ldap-terms
    
    Replace this misnomer everywhere - in code and docs.
    
    Squashed-in is a general reformatting and expansion of the README,
    including mention of a previously-undocumented `LDAP_SEARCH_FILTER`
    plugin configuration parameter.
    
    Add a reminder to use `pip3` if necessary (happened previously:

--- taiga_contrib_ldap_auth/connector.py
+++ taiga_contrib_ldap_auth/connector.py
@@ -29,14 +29,14 @@ SERVER = getattr(settings, "LDAP_SERVER", "")
 PORT = getattr(settings, "LDAP_PORT", "")
 
 SEARCH_BASE = getattr(settings, "LDAP_SEARCH_BASE", "")
-SEARCH_PROPERTY = getattr(settings, "LDAP_SEARCH_PROPERTY", "")
-SEARCH_SUFFIX = getattr(settings, "LDAP_SEARCH_SUFFIX", "")
+SEARCH_ATTRIBUTE = getattr(settings, "LDAP_SEARCH_ATTRIBUTE", "")
+SEARCH_ATTRIBUTE_SUFFIX = getattr(settings, "LDAP_SEARCH_ATTRIBUTE_SUFFIX", "")
 SEARCH_FILTER = getattr(settings, "LDAP_SEARCH_FILTER", "")
 BIND_DN = getattr(settings, "LDAP_BIND_DN", "")
 BIND_PASSWORD = getattr(settings, "LDAP_BIND_PASSWORD", "")
 
-EMAIL_PROPERTY = getattr(settings, "LDAP_EMAIL_PROPERTY", "")
-FULL_NAME_PROPERTY = getattr(settings, "LDAP_FULL_NAME_PROPERTY", "")
+EMAIL_ATTRIBUTE = getattr(settings, "LDAP_EMAIL_ATTRIBUTE", "")
+FULL_NAME_ATTRIBUTE = getattr(settings, "LDAP_FULL_NAME_ATTRIBUTE", "")
 
 def login(login: str, password: str) -> tuple:
     """
@@ -72,22 +72,24 @@ def login(login: str, password: str) -> tuple:
         raise LDAPLoginError({"error_message": error})
 
     try:
-        if(SEARCH_SUFFIX is not None and SEARCH_SUFFIX != ''):
-            search_filter = '(%s=%s)' % (SEARCH_PROPERTY, login + SEARCH_SUFFIX)
+        if(SEARCH_ATTRIBUTE_SUFFIX is not None and SEARCH_ATTRIBUTE_SUFFIX != ''):
+            search_filter = '(%s=%s)' % (SEARCH_ATTRIBUTE, login + SEARCH_ATTRIBUTE_SUFFIX)
         else:
-            search_filter = '(%s=%s)' % (SEARCH_PROPERTY, login)
+            search_filter = '(%s=%s)' % (SEARCH_ATTRIBUTE, login)
+
         if SEARCH_FILTER:
             search_filter = '(&%s(%s))' % (search_filter, SEARCH_FILTER)
+
         c.search(search_base = SEARCH_BASE,
                  search_filter = search_filter,
                  search_scope = SUBTREE,
-                 attributes = [EMAIL_PROPERTY,FULL_NAME_PROPERTY],
+                 attributes = [EMAIL_ATTRIBUTE,FULL_NAME_ATTRIBUTE],
                  paged_size = 5)
 
         if len(c.response) > 0:
             dn = c.response[0].get('dn')
-            user_email = c.response[0].get('raw_attributes').get(EMAIL_PROPERTY)[0].decode('utf-8')
-            full_name = c.response[0].get('raw_attributes').get(FULL_NAME_PROPERTY)[0].decode('utf-8')
+            user_email = c.response[0].get('raw_attributes').get(EMAIL_ATTRIBUTE)[0].decode('utf-8')
+            full_name = c.response[0].get('raw_attributes').get(FULL_NAME_ATTRIBUTE)[0].decode('utf-8')
 
             user_conn = Connection(server, auto_bind = True, client_strategy = SYNC, user = dn, password = password, authentication = SIMPLE, check_names = True)
 
