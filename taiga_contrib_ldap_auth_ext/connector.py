c671580c3ab748519498bc99f65cae7720dc83a0
madmath03 <mb.mathieu.brunot@gmail.com>
2018-04-05 16:43:44 +0200

    Fix #6 by converting dn to utf-8

--- taiga_contrib_ldap_auth_ext/connector.py
+++ taiga_contrib_ldap_auth_ext/connector.py
@@ -125,7 +125,7 @@ def login(login: str, password: str) -> tuple:
     email = c.response[0].get('raw_attributes').get(EMAIL_ATTRIBUTE)[0].decode('utf-8')
     full_name = c.response[0].get('raw_attributes').get(FULL_NAME_ATTRIBUTE)[0].decode('utf-8')
     try:
-        dn = str(bytes(c.response[0].get('dn'), 'iso-8859-1'), encoding='utf-8')
+        dn = str(bytes(c.response[0].get('dn'), 'utf-8'), encoding='utf-8')
         user_conn = Connection(server, auto_bind = auto_bind, client_strategy = SYNC,
                                check_names = True, authentication = SIMPLE,
                                user = dn, password = password)
